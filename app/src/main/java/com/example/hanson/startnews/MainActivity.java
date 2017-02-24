d7cbdc8015a3fc54b104a47ace232a45a3f01d0a
hanson <hansonofon@gmail.com>
2017-02-24 17:20:58 +0100

    feat: Added Adapter and UI inflated

--- app/src/main/java/com/example/hanson/startnews/MainActivity.java
+++ app/src/main/java/com/example/hanson/startnews/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.hanson.startnews;
 
+
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.hanson.startnews.net.RSSParser;
 import com.example.hanson.startnews.services.SyncUtils;
+import com.example.hanson.startnews.utils.Connection;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>,
+        NavigationView.OnNavigationItemSelectedListener{
+    private static final String TAG = "MainActivity";
+    public Context nContext;
     private boolean mTwoPane;
+    private RecyclerView mRecyclerView;
+    boolean isConnected;
 
-    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +57,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             mTwoPane = true;
         }
 
+        RSSParser.link = "http://www.techrepublic.com/mediafed/articles/latest/";
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         SyncUtils.CreateSyncAccount(this);
+
+        isConnected = Connection.isNetworkAvailable(this);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        getSupportLoaderManager().initLoader(0, null, this);
+
+        nContext = this;
+        loadNews();
 
 
 
@@ -106,6 +136,170 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    public void onPause() {
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+
+
+
+    @Override
+    public void onDestroy() {
+
+
+        super.onDestroy();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return MyLoader.newAllArticlesInstance(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        Adapter adapter = new Adapter(cursor);
+        adapter.setHasStableIds(true);
+        mRecyclerView.setAdapter(adapter);
+        LinearLayoutManager lm =
+                new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(lm);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mRecyclerView.setAdapter(null);
+    }
+
+    private void loadNews() {
+
+
+            SyncUtils.TriggerRefresh();
+
+
+    }
+
+
+// Recycler View Adapter
+
+    private class Adapter extends RecyclerView.Adapter<ViewHolder>  {
+
+
+
+
+        private Cursor mCursor ;
+
+
+        public Adapter(Cursor cursor) {
+            mCursor = cursor;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            mCursor.moveToPosition(position);
+            return mCursor.getLong(MyLoader.Query.COLUMN_ID);
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.news_item, parent, false);
+
+            final ViewHolder vh = new ViewHolder(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+
+                }
+            });
+
+
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            mCursor.moveToPosition(position);
+            holder.titleView.setText(mCursor.getString(MyLoader.Query.COLUMN_TITLE));
+            holder.description.setText(mCursor.getString(MyLoader.Query.COLUMN_DESC));
+            holder.pubDate.setText(mCursor.getString(MyLoader.Query.COLUMN_PUB_DATE));
+
+
+            if(mCursor.getString(MyLoader.Query.COLUMN_PHOTO_URL)==null){
+                Glide.with(holder.thumbnailView.getContext()).load(mCursor.getString(
+                        MyLoader.Query.COLUMN_PHOTO_URL2))
+
+                        //load images as bitmaps to get fixed dimensions
+                        .asBitmap()
+
+                        //set a placeholder image
+                        .placeholder(R.mipmap.ic_launcher)
+
+                        //disable cache to avoid garbage collection that may produce crashes
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .into(holder.thumbnailView);
+
+
+            }else{
+
+                Glide.with(holder.thumbnailView.getContext()).load(mCursor.getString(
+                        MyLoader.Query.COLUMN_PHOTO_URL))
+
+                        //load images as bitmaps to get fixed dimensions
+                        .asBitmap()
+
+                        //set a placeholder image
+                        .placeholder(R.mipmap.ic_launcher)
+
+                        //disable cache to avoid garbage collection that may produce crashes
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .into(holder.thumbnailView);
+
+            }
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCursor.getCount();
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView thumbnailView;
+        public TextView titleView;
+        public TextView description;
+        public TextView pubDate;
+
+        public ViewHolder(View view) {
+            super(view);
+            thumbnailView = (ImageView) view.findViewById(R.id.image_view);
+            titleView = (TextView) view.findViewById(R.id.title_view);
+            description = (TextView) view.findViewById(R.id.description_view);
+            pubDate = (TextView) view.findViewById(R.id.date_view);
+        }
     }
 
 
---
+++ app/src/main/java/com/example/hanson/startnews/MyLoader.java
@@ -0,0 +1,45 @@
+package com.example.hanson.startnews;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.v4.content.CursorLoader;
+import com.example.hanson.startnews.db.FeedContract;
+
+/**
+ * Created by hanson on 2/24/17.
+ */
+
+public class MyLoader extends CursorLoader {
+
+    public static MyLoader newAllArticlesInstance(Context context) {
+        return new MyLoader(context, FeedContract.Entry.buildDirUri());
+    }
+
+    public static MyLoader newInstanceForItemId(Context context, long itemId) {
+        return new MyLoader(context, FeedContract.Entry.buildItemUri(itemId));
+    }
+
+    private MyLoader(Context context, Uri uri) {
+        super(context, uri, Query.PROJECTION, null, null, FeedContract.Entry.COLUMN_NAME_PUBLISHED+ " DESC");
+    }
+
+    public interface Query {
+        String[] PROJECTION = {
+                FeedContract.Entry._ID,
+                FeedContract.Entry.COLUMN_NAME_TITLE,
+                FeedContract.Entry.COLUMN_NAME_PUBLISHED,
+                FeedContract.Entry.COLUMN_NAME_LINK,
+                FeedContract.Entry.COLUMN_NAME_DESCRIPTION,
+                FeedContract.Entry.COLUMN_NAME_IMAGE_URL,
+                FeedContract.Entry.COLUMN_NAME_IMAGE_URL_2,
+        };
+
+        int COLUMN_ID = 0;
+        int COLUMN_TITLE = 1;
+        int COLUMN_PUB_DATE = 2;
+        int COLUMN_LINK = 3 ;
+        int COLUMN_DESC = 4;
+        int COLUMN_PHOTO_URL = 5;
+        int COLUMN_PHOTO_URL2 = 6;
+    }
+}
