234c50ccf8c71b8ac56a0eb286a7fd687fb171bf
hanson <hansonofon@gmail.com>
2017-02-24 16:38:48 +0100

    feat: Added Feed Parser

---
+++ app/src/main/java/com/example/hanson/startnews/net/RSSItem.java
@@ -0,0 +1,81 @@
+package com.example.hanson.startnews.net;
+
+/**
+ * Created by hanson on 2/24/17.
+ */
+
+public class RSSItem {
+
+    String title;
+    String description;
+    String pubDate;
+    String com_description;
+    String imageUrl2;
+    String guid;
+    String link;
+    String imageUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getImageUrl2() {
+        return imageUrl2;
+    }
+
+    public void setImageUrl2(String imageUrl2) {
+        this.imageUrl2 = imageUrl2;
+    }
+
+    public String getCom_description() {
+        return com_description;
+    }
+
+    public void setCom_description(String com_description) {
+        this.com_description = com_description;
+    }
+
+}
---
+++ app/src/main/java/com/example/hanson/startnews/net/RSSParser.java
@@ -0,0 +1,94 @@
+package com.example.hanson.startnews.net;
+
+import android.util.Log;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hanson on 2/24/17.
+ */
+
+public class RSSParser {
+
+    public static String link ;
+
+
+    public List<RSSItem> parse() {
+        List<RSSItem> itemsList = null;
+        XmlPullParser parser = Xml.newPullParser();
+        try {
+            // auto-detect the encoding from the stream
+            parser.setInput(getInputStream(), null);
+            int eventType = parser.getEventType();
+            RSSItem rssItem = null;
+            boolean done = false;
+            while (eventType != XmlPullParser.END_DOCUMENT && !done) {
+                String name = null;
+                String attributeValue = null;
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        itemsList = new ArrayList<RSSItem>();
+                        break;
+                    case XmlPullParser.START_TAG:
+                        name = parser.getName();
+
+                        if (name.equalsIgnoreCase("item")) {
+                            rssItem = new RSSItem();
+                        } else if (rssItem != null) {
+                            if (name.equalsIgnoreCase("description")) {
+                                rssItem.setDescription(parser.nextText());
+
+                            } else if (name.equalsIgnoreCase("pubDate")) {
+                                rssItem.setPubDate(parser.nextText());
+                            } else if (name.equalsIgnoreCase("link")) {
+                                rssItem.setLink(parser.nextText());
+                            } else if (name.equalsIgnoreCase("guid")) {
+                                rssItem.setLink(parser.nextText());
+                            } else if (name.equalsIgnoreCase("title")) {
+                                rssItem.setTitle(parser.nextText());
+                            } else if (name.equalsIgnoreCase("content:encoded")) {
+                                rssItem.setImageUrl2(parser.getAttributeValue(null, "src"));
+                            }else if (name.equalsIgnoreCase("enclosure")) {
+                                rssItem.setImageUrl(parser.getAttributeValue(null, "url"));
+                            }
+
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        name = parser.getName();
+                        if (name.equalsIgnoreCase("item") && rssItem != null) {
+                            assert itemsList != null;
+                            itemsList.add(rssItem);
+                        } else if (name.equalsIgnoreCase("channel")) {
+                            done = true;
+                        }
+                        break;
+                }
+                eventType = parser.next();
+            }
+        } catch (Exception e) {
+            Log.e("PullFeedParser", e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+        return itemsList;
+    }
+
+    private InputStream getInputStream() {
+        try {
+            URL url = new URL(link);
+            return url.openConnection().getInputStream();
+        } catch (IOException e) {
+            Log.w("TAG", "Exception while retrieving the input stream", e);
+            return null;
+        }
+    }
+
+
+}
